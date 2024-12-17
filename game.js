683a15fd866e4a69f31c33b8b0142d84003b9190
itu-itis24-ozcanar23 <ozcanar23@itu.edu.tr>
2024-12-17 21:44:30 +0300

    Added score and lives in html and javascript, created alert for lose condition.

--- game.js
+++ game.js
@@ -1,13 +1,24 @@
 word = "PRISM";
+lives = 3;
+score = 0;
+
 textInputField = document.getElementById("input-field");
 letterImages = document.getElementsByClassName("letter-image");
 resetGame();
+
+function updateTexts() {
+  document.getElementById("lives-text").innerHTML = lives;
+  document.getElementById("score-text").innerHTML = score;
+}
 
 function resetGame() {
   for (let i = 0; i < letterImages.length; i++) {
     const letterImage = letterImages[i];
     letterImage.style.visibility = "hidden";
   }
+  lives = 3;
+  score = 0;
+  updateTexts();
 }
 
 function revealLetter(letterIndex) {
@@ -26,5 +37,19 @@ function getLetterIndexOf(letter) {
 function onSubmit() {
   let input = textInputField.value;
   let letterIndex = getLetterIndexOf(input);
-  revealLetter(letterIndex);
+
+  // -1 is for invalid letter
+  if (letterIndex == -1) {
+    lives--;
+  } else if (letterImages[letterIndex].style.visibility == "hidden") {
+    score += 20;
+    revealLetter(letterIndex);
+  }
+
+  updateTexts();
+
+  if (lives == 0) {
+    alert("You lost!");
+    resetGame();
+  }
 }
